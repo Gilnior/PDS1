28589f029c657fae2a28d636701bafaf18af7422
Gilnior <gilnior123@gmail.com>
2023-08-27 02:37:50 -0300

    Initial commit

---
+++ pratica0/ex1.c
@@ -0,0 +1,6 @@
+# include <stdio.h>
+
+int main() {
+    printf("Gilmar Júnio de Macêdo Guedes");
+    return 0;
+}
---
+++ pratica0/ex3.c
@@ -0,0 +1,9 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(){
+    float sin1 = sin(3.14);
+    float sin2 = sin(4.13);
+
+    printf("The sin of 3.14 is %f and the sin of 4.13 is %f", sin1, sin2);
+}
